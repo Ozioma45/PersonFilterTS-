6c827702e938df8c2f6b25d9bb682633ad241c75
Ozioma45 <oziomaegole@gmail.com>
2025-02-23 11:47:07 +0100

    create a union type Person, add sample person Data and created Criteria utility type to exclude 'type' field

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,16 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.persons = void 0;
+exports.persons = [
+    {
+        type: "user",
+        name: "Paul Schwarzmüller",
+        age: 25,
+        occupation: "Clothing owner",
+    },
+    { type: "admin", name: "Alex Smith", age: 18, role: "Administrator" },
+    { type: "user", name: "Alice Miles", age: 30, occupation: "Accountant" },
+    { type: "admin", name: "Steve Miller", age: 40, role: "Head Admin" },
+    { type: "user", name: "Victor M.", age: 20, occupation: "Tailor" },
+    { type: "admin", name: "Alex", age: 28, role: "Civil Engineer" },
+];
--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,21 @@ interface Admin {
   age: number;
   role: string;
 }
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+  {
+    type: "user",
+    name: "Paul Schwarzmüller",
+    age: 25,
+    occupation: "Clothing owner",
+  },
+  { type: "admin", name: "Alex Smith", age: 18, role: "Administrator" },
+  { type: "user", name: "Alice Miles", age: 30, occupation: "Accountant" },
+  { type: "admin", name: "Steve Miller", age: 40, role: "Head Admin" },
+  { type: "user", name: "Victor M.", age: 20, occupation: "Tailor" },
+  { type: "admin", name: "Alex", age: 28, role: "Civil Engineer" },
+];
+
+type Criteria<T> = Partial<Omit<T, "type">>;
